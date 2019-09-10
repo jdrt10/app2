d3385313ae2a1a9141db60b94dcd257c129a41b2
JDash42 <triggeredjd@gmail.com>
2019-09-10 07:02:41 +0000

    added has_many to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  has_many :annoucements, dependent: :destroy,
+                          class_name: "Announcement",
+                          foreign_key: "annoucement_id"
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
